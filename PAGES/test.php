f9ee757a5e7ecca834af4f321328c8d5c3eaf34d
rogos1337 <rogos1337@gmail.com>
2022-11-17 13:44:11 +0100

    1

--- PAGES/test.php
+++ PAGES/test.php
@@ -1,1 +1,8 @@
+<h1>TEST POŁĄCZENIA Z SERWEREM <i><?=$servername ?></i> I BAZĄ<i><?=$database ?></i></h1>
+
+<p>
 <?php
+    echo 'Host info: ' . mysqli_get_host_info($mysqliProceduralConnection) . '<br>';
+    echo 'MySQLi stat: ' . mysqli_stat($mysqliProceduralConnection);
+    ?>
+</p>
