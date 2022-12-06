5906086d647784f0bcab21dc2a68eb8df287b23b
rogos1337 <rogos1337@gmail.com>
2022-12-06 10:11:10 +0100

    as

--- PAGES/czytelnicy.php
+++ PAGES/czytelnicy.php
@@ -1,27 +1,13 @@
-
 <?php
 $query = 'SELECT * FROM czytelnicy';
 $result = mysqli_query($conn, $query);
+if (mysqli_num_rows($result) > 0){
+    echo '<table>';
+    echo '<tr><th>Nr Czytelnika</th><th>Imię i Nazwisko</th><th>Data urodzenia</th><th>Adres</th><th>Data Zapisania</th><th>Data Skreślenia</th><th>Numer Legitymacji</th><th>Funkcja</th><th>Płeć</th></tr>';
+    while ($row = mysqli_fetch_assoc($result)){
+        echo '<tr><td>' . $row['Nr_czytelnika'] . '</td><td>' . $row['Imie'] . ' ' . $row['Nazwisko'] . '</td><td>' . $row['Data_ur'] . '</td><td>' . $row['Ulica'] . ' ' . $row['Kod'] . ' ' . $row['Miasto'] . '</td><td>' . $row['Data_zapisania'] . '</td><td>' . $row['Data_skreslenia'] . '</td><td>' . $row['Nr_legitymacji'] . '</td><td>' . $row['Funkcja'] . '</td><td>' . $row['Plec'] . '</td></tr>';
+    }
+    echo '</table>';
+}
 ?>
-<table>
-    <tr>
-        <?php
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)){
-                echo '<tr><td>' . $row['Nr_czytelnika'] . '</td><td>'
-                    . $row['Nazwisko'] . '</td><td>'
-                    . $row['Imie'] . '</td><td>'
-                    . $row['Data_ur'] . '</td><td>'
-                    . $row['Ulica'] . '</td><td>'
-                    . $row['Kod'] . '</td><td>'
-                    . $row['Miasto'] . '</td><td>'
-                    . $row['Data_zapisania'] . '</td><td>'
-                    . $row['Data_skreslenia'] . '</td><td>'
-                    . $row['Nr_legitymacji'] . '</td><td>'
-                    . $row['Funkcja'] . '</td><td>'
-                    . $row['Plec'] . '</td></tr>';
-            }
-        }
-        ?>
-</table>
 
